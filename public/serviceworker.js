050d1c018d57fd5b10c7b81b8fa080445fa231cc
ZooWee <condreaychris@gmail.com>
2021-11-29 13:46:22 -0800

    event listeners in seriveworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -8,3 +8,35 @@ const FILES_TO_CACHE = [
   'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
+
+const PRECACHE = 'precache-v1'
+const RUNTIME = 'runtime';
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  )
+})
+
+// event lsitener to prevent and delete other caches
+self.addEventListener('activate', (e) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete)
+          })
+        )
+      })
+      .then(() => self.clients.claim())
+  )
+})
